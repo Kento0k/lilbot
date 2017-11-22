3f107a0f5f69d5c68ebc768825614d476ba3e1f7
Helnsidva <heln.sidva@gmail.com>
2017-11-22 22:31:39 +0300

    init

--- src/main/kotlin/ru/spbstu/competition/game/Intellect.kt
+++ src/main/kotlin/ru/spbstu/competition/game/Intellect.kt
@@ -10,10 +10,10 @@ class Intellect(val state: State, val protocol: Protocol) {
         // Da best strategy ever!
 
         // If there is a free river near a mine, take it!
-//        val try0 = state.rivers.entries.find { (river, riverState) ->
-//            riverState == RiverState.Neutral && (river.source in state.mines || river.target in state.mines)
-//        }
-//        if(try0 != null) return protocol.claimMove(try0.key.source, try0.key.target)
+        val try0 = state.rivers.entries.find { (river, riverState) ->
+            riverState == RiverState.Neutral && (river.source in state.mines || river.target in state.mines)
+        }
+        if(try0 != null) return protocol.claimMove(try0.key.source, try0.key.target)
 
         if(state.currentWay.isEmpty()) {
 
@@ -34,43 +34,57 @@ class Intellect(val state: State, val protocol: Protocol) {
                     }
                 }
             }
-            if(doWay.size != 0) {
-                val try0 = state.rivers.entries.find { (river, riverState) ->
+            if (doWay.size != 0) {
+                for (oneWay in doWay) {
+                    val try0 = state.rivers.entries.find { (river, riverState) ->
+                        riverState == RiverState.Neutral && (river.source in state.mines || river.target in state.mines)
+                        && (oneWay.key == river.source || oneWay.key == river.target
+                                || oneWay.value == river.source || oneWay.value == river.target)
+                    }
+                    if (try0 != null) {
+                        doWay.remove(oneWay.key)
+                        return protocol.claimMove(try0.key.source, try0.key.target)
+                    }
+                }
+            }
+            else {
+
+                val try1 = state.rivers.entries.find { (river, riverState) ->
                     riverState == RiverState.Neutral && (river.source in state.mines || river.target in state.mines)
                 }
-                if(try0 != null) return protocol.claimMove(try0.key.source, try0.key.target)
+                if(try1 != null) return protocol.claimMove(try1.key.source, try1.key.target)
+
+                val ourSites = state
+                        .rivers
+                        .entries
+                        .filter { it.value == RiverState.Our }
+                        .flatMap { listOf(it.key.source, it.key.target) }
+                        .toSet()
+
+                // If there is a river between two our pointsees, take it!
+                val try2 = state.rivers.entries.find { (river, riverState) ->
+                    riverState == RiverState.Neutral && (river.source in ourSites && river.target in ourSites)
+                }
+                if(try2 != null) return protocol.claimMove(try2.key.source, try2.key.target)
+
+                // If there is a river near our pointsee, take it!
+                val try3 = state.rivers.entries.find { (river, riverState) ->
+                    riverState == RiverState.Neutral && (river.source in ourSites || river.target in ourSites)
+                }
+                if(try3 != null) return protocol.claimMove(try3.key.source, try3.key.target)
+
+                // Bah, take anything left
+                val try4 = state.rivers.entries.find { (_, riverState) ->
+                    riverState == RiverState.Neutral
+                }
+                if (try4 != null) return protocol.claimMove(try4.key.source, try4.key.target)
+
+
             }
 
         }
-
-        // Look at all our pointsees
-        val ourSites = state
-                .rivers
-                .entries
-                .filter { it.value == RiverState.Our }
-                .flatMap { listOf(it.key.source, it.key.target) }
-                .toSet()
-
-        // If there is a river between two our pointsees, take it!
-        val try1 = state.rivers.entries.find { (river, riverState) ->
-            riverState == RiverState.Neutral && (river.source in ourSites && river.target in ourSites)
-        }
-        if(try1 != null) return protocol.claimMove(try1.key.source, try1.key.target)
-
-        // If there is a river near our pointsee, take it!
-        val try2 = state.rivers.entries.find { (river, riverState) ->
-            riverState == RiverState.Neutral && (river.source in ourSites || river.target in ourSites)
-        }
-        if(try2 != null) return protocol.claimMove(try2.key.source, try2.key.target)
-
-        // Bah, take anything left
-        val try3 = state.rivers.entries.find { (_, riverState) ->
-            riverState == RiverState.Neutral
-        }
-        if (try3 != null) return protocol.claimMove(try3.key.source, try3.key.target)
-
-        // (╯°□°)╯ ┻━┻
         protocol.passMove()
+
     }
 
 }
