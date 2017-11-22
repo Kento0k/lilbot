1d5a5d92ee863a9ba9b6755cf9a69b522e038154
Helnsidva <heln.sidva@gmail.com>
2017-11-22 07:22:23 +0300

    init

--- src/main/kotlin/ru/spbstu/competition/game/State.kt
+++ src/main/kotlin/ru/spbstu/competition/game/State.kt
@@ -8,10 +8,17 @@ enum class RiverState{ Our, Enemy, Neutral }
 
 class State {
     class Pairs(val x: Double?, val y: Double?)
+    class Component() {
+        var sited = mutableSetOf<Int>()
+        fun addSite(argSite: Int) {
+            sited.add(argSite)
+        }
+    }
     val rivers = mutableMapOf<River, RiverState>()
     var mines = listOf<Int>()
     var myId = -1
-    val coordinates = mutableMapOf<Int, Pairs>()
+    var coordinates = mutableMapOf<Int, Pairs>()
+    var linkComponents = mutableSetOf<Component>()
 
     fun init(setup: Setup) {
         myId = setup.punter
@@ -20,6 +27,7 @@ class State {
         }
         for(river in setup.map.rivers) {
             rivers[river] = RiverState.Neutral
+
         }
         for(mine in setup.map.mines) {
             mines += mine
